b9acc1448a36de64308b3aae083a1868eee8d087
Rachel Moser <moser.rachel@gmail.com>
2019-12-15 14:31:52 -0600

    one round logic started

--- script.js
+++ script.js
@@ -8,45 +8,89 @@ function computerPlay() {
     else return 'Paper';
 }
 
-let computerScore = 0;
-let playerScore = 0;
+// let computerScore = 0;
+// let playerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = this.textContent.toLowerCase();
+    computerSelection = computerPlay();
+    console.log(`player: ${playerSelection}`);
+    console.log(`computer: ${computerSelection}`);
     if (playerSelection == 'rock' && computerSelection == 'Scissors') {
-        playerScore++;
-        return `You Win! Rock beats Scissors. Score: ${playerScore} to ${computerScore}`;
+        console.log( `You Win! Rock beats Scissors.`);
     } else if (playerSelection == 'rock' && computerSelection == 'Paper') {
-        computerScore++;
-        return `You Lose! Paper covers rock. Score: ${playerScore} to ${computerScore}`;
+        console.log( `You Lose! Paper covers rock`);
     } else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
-        computerScore++;
-        return `You Lose! Scissors cut paper. Score: ${playerScore} to ${computerScore}`
+        console.log( `You Lose! Scissors cut paper`);
     } else if (playerSelection == 'paper' && computerSelection == 'Rock') {
-        playerScore++;
-        return `You Win! Paper covers rock. Score: ${playerScore} to ${computerScore}`
+        console.log( `You Win! Paper covers rock`);
     } else if (playerSelection == 'scissors' && computerSelection == "Rock") {
-        computerScore++;
-        return `You Lose! Scissors beats rock. Score: ${playerScore} to ${computerScore}`
+        console.log( `You Lose! Scissors beats rock`);
     } else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
-        playerScore++;
-        return `You Win! Scissors beat paper. Score: ${playerScore} to ${computerScore}`
+        console.log( `You Win! Scissors beat paper`);
     }
-    else return `That was a draw. Try Again! Score: ${playerScore} to ${computerScore}`;
+    else console.log( `That was a draw. Try Again!`);
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Type: Rock, Paper, or Scissors')
-        playerSelection = playerSelection.toLowerCase();
-        const computerSelection = computerPlay()
-        console.log(playRound(playerSelection, computerSelection))
-    }
-    if (playerScore > computerScore) {
-        console.log(`You're the winner! Final Score: ${playerScore} to ${computerScore}`);
-    } else if (computerScore > playerScore) {
-        console.log(`You're the loser. Final Score: ${playerScore} to ${computerScore}`);
-    } else
-        console.log(`You tied. Final Score: ${playerScore} to ${computerScore}`);
-}
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
 
-game();
+// function playerButton() {
+//     // playerSelection = this.textContent;
+//     console.log(this.textContent)
+// }
+
+rockButton.addEventListener('click', playRound);
+paperButton.addEventListener('click', playRound);
+scissorsButton.addEventListener('click', playRound);
+
+// function playRound(playerSelection, computerPlay) {
+//     if (playerSelection == 'rock' && computerPlay == 'Scissors') {
+//         playerScore++;
+//         return `You Win! Rock beats Scissors. Score: ${playerScore} to ${computerScore}`;
+//     } else if (playerSelection == 'rock' && computerPlay == 'Paper') {
+//         computerScore++;
+//         return `You Lose! Paper covers rock. Score: ${playerScore} to ${computerScore}`;
+//     } else if (playerSelection == 'paper' && computerPlay == 'Scissors') {
+//         computerScore++;
+//         return `You Lose! Scissors cut paper. Score: ${playerScore} to ${computerScore}`
+//     } else if (playerSelection == 'paper' && computerPlay == 'Rock') {
+//         playerScore++;
+//         return `You Win! Paper covers rock. Score: ${playerScore} to ${computerScore}`
+//     } else if (playerSelection == 'scissors' && computerPlay == "Rock") {
+//         computerScore++;
+//         return `You Lose! Scissors beats rock. Score: ${playerScore} to ${computerScore}`
+//     } else if (playerSelection == 'scissors' && computerPlay == 'Paper') {
+//         playerScore++;
+//         return `You Win! Scissors beat paper. Score: ${playerScore} to ${computerScore}`
+//     }
+//     else return `That was a draw. Try Again! Score: ${playerScore} to ${computerScore}`;
+// }
+
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = prompt('Type: Rock, Paper, or Scissors')
+//         playerSelection = playerSelection.toLowerCase();
+//         const computerPlay = computerPlay()
+//         console.log(playRound(playerSelection, computerPlay))
+//     }
+//     if (playerScore > computerScore) {
+//         console.log(`You're the winner! Final Score: ${playerScore} to ${computerScore}`);
+//     } else if (computerScore > playerScore) {
+//         console.log(`You're the loser. Final Score: ${playerScore} to ${computerScore}`);
+//     } else
+//         console.log(`You tied. Final Score: ${playerScore} to ${computerScore}`);
+// }
+
+// game();
+
+// x Copy your original code into a new file so you don’t lose it.
+// x For now, remove the logic that plays exactly five rounds.
+// Create three buttons, one for each selection. Add an event listener to the buttons that calls your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+// Add a div for displaying results and change all of your console.logs into DOM methods.
+// Display the running score, and announce a winner of the game once one player reaches 5 points.
+// You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of the programmer’s life.
+// Don’t forget to go back and add your new version to the original Rock Paper Scissors Project!
+
