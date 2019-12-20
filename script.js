df5605982d52c2af8da3c8a242fe3dc38724659b
Rachel Moser <moser.rachel@gmail.com>
2019-12-19 19:20:25 -0600

    logic to reset animation

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let computerScore = 0;
 let playerScore = 0;
 
+// Naming User and Computer Scores to update
+const userScore = document.querySelector('.user-score');
+const randomScore = document.querySelector('.computer-score');
+
+// Naming User's Buttons to add event listeners
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+
+// Naming Computer's Buttons to add event listeners
+const computerRock = document.querySelector('.computer-rock');
+const computerPaper = document.querySelector('.computer-paper');
+const computerScissors = document.querySelector('.computer-scissors');
+
+// Naming and Placing the "Results" of each round
 const resultDiv = document.createElement('div');
 const results = document.createElement('p');
 resultDiv.appendChild(results);
@@ -8,14 +23,7 @@ resultDiv.classList.add('round-result');
 const userContainer = document.querySelector('.user-container');
 userContainer.insertAdjacentElement('afterend', resultDiv);
 
-const rockButton = document.querySelector('.rock');
-const paperButton = document.querySelector('.paper');
-const scissorsButton = document.querySelector('.scissors');
-
-const computerRock = document.querySelector('.computer-rock');
-const computerPaper = document.querySelector('.computer-paper');
-const computerScissors = document.querySelector('.computer-scissors');
-
+// Creating a equally random outcome - rock, paper, or scissors
 function computerPlay() {
         const number = Math.floor(Math.random() * 1000);
         if (number % 3 === 0) {
@@ -27,25 +35,53 @@ function computerPlay() {
         return 'scissors';
 }
 
+// Adding animation to the Computer's "Button"
 function computerColor(computerSelection) {
         if (computerSelection === 'rock') {
+                removeColor();
                 computerRock.classList.add('computerPick');
         }
         if (computerSelection === 'paper') {
+                removeColor();
                 computerPaper.classList.add('computerPick');
         }
         if (computerSelection === 'scissors') {
+                removeColor();
                 computerScissors.classList.add('computerPick');
         }
 }
 
+// Removing color from the Computer's "Button"
+function removeColor() {
+        computerRock.classList.remove('computerPick');
+        computerPaper.classList.remove('computerPick');
+        computerScissors.classList.remove('computerPick');
+}
+
+// Removing color from the User's "Button"
+function removeUserColor() {
+        rockButton.classList.remove('userClick');
+        paperButton.classList.remove('userClick');
+        scissorsButton.classList.remove('userClick');
+}
+
+// Adding :hover state to User's Button
+function userHover() {
+        if (playerScore <= 4 && computerScore <= 4) {
+                this.classList.add('userHover');
+        }
+}
+
+// Removing :hover state from the User's Button
+function removeHover() {
+        this.classList.remove('userHover');
+}
+
 function playGame(playerSelection, computerSelection) {
-        const buttonChoice = this.dataset.button;
-        playerSelection = buttonChoice;
-        this.classList.add('userClick');
+        playerSelection = this.dataset.button;
         computerSelection = computerPlay();
-        const userScore = document.querySelector('.user-score');
-        const randomScore = document.querySelector('.computer-score');
+        removeUserColor(); // Removes any current animation
+        this.classList.add('userClick'); // Adds new button animation
         if (
                 (playerSelection === 'rock' && computerSelection === 'scissors') ||
                 (playerSelection === 'paper' && computerSelection === 'rock') ||
@@ -54,8 +90,8 @@ function playGame(playerSelection, computerSelection) {
                         (computerScore <= 5 || playerScore <= 5))
         ) {
                 results.textContent = `You won, because ${playerSelection} beats ${computerSelection}.`;
-                playerScore++;
-                userScore.textContent = `${playerScore}`;
+                playerScore++; // New Score
+                userScore.textContent = `${playerScore}`; // Updates Score
                 if (playerScore >= 5) {
                         results.textContent = `Congrats! You won the FINAL ROUND, because ${playerSelection} beats ${computerSelection}. Refresh to try again!`;
                         rockButton.removeEventListener('click', playGame);
@@ -70,8 +106,8 @@ function playGame(playerSelection, computerSelection) {
                         (computerScore <= 5 || playerScore <= 5))
         ) {
                 results.textContent = `You lost, because ${computerSelection} beats ${playerSelection}.`;
-                computerScore++;
-                randomScore.textContent = `${computerScore}`;
+                computerScore++; // Computer Score
+                randomScore.textContent = `${computerScore}`; // Updates Score
                 if (computerScore >= 5) {
                         results.textContent = `You lost the final round, because ${computerSelection} beats ${playerSelection}. GAME OVER, but you can refresh to try again!`;
                         rockButton.removeEventListener('click', playGame);
@@ -81,52 +117,43 @@ function playGame(playerSelection, computerSelection) {
         } else {
                 results.textContent = `It's a tie. The computer picked ${computerSelection} too. Try Again!`;
         }
-        computerColor(computerSelection);
+        computerColor(computerSelection); // Adds animation for Computer's "Button"
 }
 
-function removeColor() {
-        computerRock.classList.remove('computerPick');
-        computerPaper.classList.remove('computerPick');
-        computerScissors.classList.remove('computerPick');
-}
-
-function userHover() {
-        if (playerScore <= 4 && computerScore <= 4) {
-                this.classList.add('userHover');
-        }
-}
-
-function removeHover() {
-        this.classList.remove('userHover');
-}
-
+// Removing animation for User's Button
 function removeClick() {
         this.classList.remove('userClick');
         this.classList.remove('userHover');
-
 }
 
+// User's Button Event Listener to Play the Game
 rockButton.addEventListener('click', playGame);
 paperButton.addEventListener('click', playGame);
 scissorsButton.addEventListener('click', playGame);
 
-computerRock.addEventListener('transitionend', removeColor);
-computerPaper.addEventListener('transitionend', removeColor);
-computerScissors.addEventListener('transitionend', removeColor);
-
+// User's Button Event Listener to remove animation
 rockButton.addEventListener('transitionend', removeClick);
 paperButton.addEventListener('transitionend', removeClick);
 scissorsButton.addEventListener('transitionend', removeClick);
 
+// User's Button Event Listener for :hover state
 rockButton.addEventListener('mouseover', userHover);
 paperButton.addEventListener('mouseover', userHover);
 scissorsButton.addEventListener('mouseover', userHover);
 
+// User's Button Event Listener to remove :hover state
 rockButton.addEventListener('mouseleave', removeHover);
 paperButton.addEventListener('mouseleave', removeHover);
 scissorsButton.addEventListener('mouseleave', removeHover);
 
+// "Computer's Buttons" to remove animation
+computerRock.addEventListener('transitionend', removeColor);
+computerPaper.addEventListener('transitionend', removeColor);
+computerScissors.addEventListener('transitionend', removeColor);
+
 // Change sizes of fonts and imgs in media queries
+// add results log
+// Make "results" better worded and formated
 // Can't click on next game until animation is over, for animation to happen.
-// Update Font Awesome to be local and not linked to their site
+// Can I check to see if an animation is running? if (this.classList.includes(userClick))
 // Don’t forget to go back and add your new version to the original Rock Paper Scissors Project!
